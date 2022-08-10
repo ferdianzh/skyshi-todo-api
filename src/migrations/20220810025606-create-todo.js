9c7a5ba8188c3e266c1d0a54b222aec15e71e59f
ferdianzh <ferdianzh30@gmail.com>
2022-08-10 10:29:47 +0700

    add migration

---
+++ src/migrations/20220810025606-create-todo.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      activity_group_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: 'activities',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN
+      },
+      priority: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    })
+    .then();
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('todos');
+  }
+};
