a34c25acd9b3bc17d5f78f5f975118a4747c8d24
ferdianzh <ferdianzh30@gmail.com>
2022-08-11 12:31:01 +0700

    set db connection

--- src/config/config.js
+++ src/config/config.js
@@ -8,6 +8,13 @@ const config = {
     host: process.env.MYSQL_HOST,
     dialect: 'mysql'
   },
+  mysql : {
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DBNAME,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT
+  },
 }
 
 module.exports = config;
